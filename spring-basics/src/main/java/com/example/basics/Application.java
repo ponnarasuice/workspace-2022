d321bad476ece4eb9093c265a4db445948c73ce6
ponnarasu <ponnarasu.mcet@gmail.com>
2022-03-15 23:09:02 +0530

    test

--- spring-basics/src/main/java/com/example/basics/Application.java
+++ spring-basics/src/main/java/com/example/basics/Application.java
@@ -12,8 +12,9 @@ public class Application {
   public static void main(String args[]) {
     // usually we create objects by new operator. spring takes care of that and stores it in context
     // need to include the spring-context jar, spring-core jar, spring beans
-    
 
+
+    
 
     // spring creates objects based on the configurations in xml or annotations
     ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
